021abd30d40ffc38b07f0502779e4cb3eaadbb86
sangldq0321 <sangldq0321@gmail.com>
2023-03-31 16:50:39 +0700

    Add scroll to top button

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -83,6 +83,7 @@
             <div class="text-center">&copy 2023 <b>UMS</b>. All rights reserved.</div>
         </div>
     </footer>
+    <button class="top-btn shadow" onclick="topFunction()" id="topBtn"><i class="fa-solid fa-chevron-up"></i></button>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
@@ -104,6 +105,20 @@ $(function () {
         }
     })
 });
+let topButton = document.getElementById("topBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 $('input').attr('autocomplete', 'off')
     </script>
     <link
